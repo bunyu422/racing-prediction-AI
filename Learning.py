54f098f957020a30b9636a0cd058a2e93d290e16
bunyu <kekeroro9302@gmail.com>
2024-02-21 18:57:12 +0900

    first commit

---
+++ Learning.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b83a44f7878be9c64a9299446b20cf2ad3fe94d0bf1556ba184f906a3ad29221
+size 34833
---
+++ Predict.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:31ac827b537883e6e47d8e7ec50ae7cd2c420cb440f2e37764e5494d8997e7e8
+size 20051
