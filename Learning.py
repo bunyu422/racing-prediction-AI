a41b134f723464562d50ab1da778f8b8cd124e2c
bunyu <kekeroro9302@gmail.com>
2024-02-21 19:19:54 +0900

    first commit

--- Learning.py
+++ Learning.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b83a44f7878be9c64a9299446b20cf2ad3fe94d0bf1556ba184f906a3ad29221
-size 34833
+oid sha256:fc5be249e3da103d009889f977d2870e78f8c7232f092f5b4f26c970c7a728c8
+size 35080
